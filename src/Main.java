e9984c0a74a7276e343aff5d089f0d23c31dd1ba
Your Name <applezafar@gmail.com>
2022-06-26 00:04:31 +1000

    homework massives

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-
 
     public static void main(String[] args) {
 
 
-        int[][] mas = new int[][]{
-                {1, 1, 1},
-                {1, 1, 1},
-                {1, 1, 1}
-        };
+    //  int[][] mas = new int[][]{
+    //     {1, 1, 1},
+    //         {1, 1, 1},
+    //      {1, 1, 1}
+    // };
+    // for (int i = 0; i < 3; i++) {
 
+    //  for (int j = 0; j < 3; j++) {
 
-        for (int i = 0; i < 3; i++) {
+    //      System.out.print(mas[i][j] + " ");
 
-            for (int j = 0; j < 3; j++) {
-                System.out.print(mas[i][j] + " ");
+    int[] arr = {5, 4, 3, 2, 1};
 
-            }
-        }
-    }
-}
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 4; i >=0; i--) {
+        System.out.println(arr[i]);}
+
+        }}
